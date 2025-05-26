372e71ca20462d19a635e8f44b86599e92286581
Oskar Blickfeldt <38707265+ofakar@users.noreply.github.com>
2025-05-26 19:13:44 +0200

    chore: Add vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/odin-rock-paper-scissors/", // Replace 'odin-rock-paper-scissors' with your actual repository name
+});
